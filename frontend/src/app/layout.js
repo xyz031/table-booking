61145acadd63ce1a8d141d22814ccd4f41eaa2f1
Aditya <adityamaurya0401@gmail.com>
2025-01-02 14:57:26 +0530

    six

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,6 @@
 "use client"; 
 
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';  // Import Toaster from react-hot-toast
 
